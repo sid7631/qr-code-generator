2f6d1ac292c82965dfa75ca19c1483ed38e6ea51
Siddharth Sarkar <sidsarkar07@gmail.com>
2023-02-01 06:56:05 +0530

    added qr code page

---
+++ app.js
@@ -0,0 +1,31 @@
+let btn = document.getElementById('generate-qr')
+let qr_code_element = document.querySelector(".qr-code")
+
+btn.addEventListener("click", () => {
+    let user_input = document.querySelector('#qr-text')
+    if(user_input.value != "") {
+        if(qr_code_element.childElementCount==0){
+            generate_qr(user_input)
+        } else {
+            qr_code_element.innerHTML = ''
+            generate_qr(user_input)
+        }
+    } else {
+        console.log("not valid")
+        qr_code_element.style = "display:none"
+    }
+})
+
+function generate_qr(user_input) {
+    qr_code_element.style = ''
+    var qrcode = new QRCode(document.querySelector(".qr-code"), {
+        text: `${user_input.value}`,
+        width: 180, //default 128
+        height: 180,
+        colorDark : "#ffffff",
+        colorLight : "#447BF1",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+}
+
+generate_qr({value:'siddharth'})
